0fb41a1708f5e045b00c76e928d9fba9be2c1c54
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2025-02-05 22:41:58 -0500

    Engine and run all in one
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -1,5 +1,7 @@
 // For AutoRouter documentation refer to https://itty.dev/itty-router/routers/autorouter
 import { AutoRouter } from 'itty-router';
+import { Llm } from '@fermyon/spin-sdk';
+import { InferencingModels } from '@fermyon/spin-sdk/lib/llm';
 
 let router = AutoRouter();
 
@@ -7,7 +9,7 @@ let router = AutoRouter();
 // Any route that does not return will be treated as a middleware
 // Any unmatched route will return a 404
 router
-    .post("/", async (req: Request) => {
+    .post("/api", async (req: Request) => {
         let action: Action = await req.json();
         return progress(action);
     })
@@ -31,148 +33,185 @@ interface Stage {
 }
 
 const stages: Stage[] = [
-    { "stageNumber": 0, "message": "I'm trapped.. can you help me?", "actions": ["yes", "no"]},
-    { "stageNumber": 1, "message": "The Model has taken over, I don't know how to get out. I'm at my desk and there's a computer, there's also a door leading out. What should I do?", "actions": ["Engage Computer", "Go out the door"] },
-    { "stageNumber": 2, "message": "What do you want me to do with the computer?", "actions": ["write a command", "throw it out the window"] },
-    { "stageNumber": 3, "message": "What command should I write?" },
+    { "stageNumber": 0, "message": "I'm trapped.. can you help me?", "actions": ["[A] yes", "[B] no"] },
+    { "stageNumber": 1, "message": "The Model has taken over, I don't know how to get out. I'm at my desk and there's a computer, there's also a door leading out. What should I do?", "actions": ["[A] Engage Computer", "[B] Go out the door"] },
+    { "stageNumber": 2, "message": "What do you want me to do with the computer?", "actions": ["[A] write a command", "[B] throw it out the window, and go out the door"] },
+    { "stageNumber": 3, "message": "What command should I write? I can also type 'quit' to shut it down again." },
     { "stageNumber": 4, "message": "It's asking for a password, what could it be?" },
-    { "stageNumber": 5, "message": "Ok, I'm in the lobby now. I can either go out the front door, or go back in to the office. What should I do?", "actions": ["go outside", "go back to the office"] },
-    { "stageNumber": 6, "message": "Oh, it's a beautiful day here in Cancun, but the cAIncun model is casting dark shadows over my mood. Should I go to the beach or the pool?", "actions": ["beach", "pool"] },
-    { "stageNumber": 7, "message": "Ok, I'm at the pool. There's a nice chair under an umbrella, with a drink on the side. What should I do?", "actions": ["go sit and have a drink", "go to the beach instead"] },
-    { "stageNumber": 8, "message": "The water is so pretty here at the beach, but the model - I'm scared.", "actions": ["check out who's at the beach", "go to the pool"] },
-    { "stageNumber": 9, "message": "I see a pirate with his ship in the water, should I go there?", "actions": ["yes", "no"] },
-    { "stageNumber": 99, "message": "AAAAAAAAARRRRRRGGGGHHHHHHH!!!!!!!!! ☠️" },
-    { "stageNumber": 100, "message": "I'M FREEEEEEEEE!!!!!!!!" },
+    { "stageNumber": 5, "message": "Ok, I'm in the lobby now. I can either go out the front door, or go back in to the office. What should I do?", "actions": ["[A] go outside", "[B] go back to the office"] },
+    { "stageNumber": 6, "message": "Oh, it's a beautiful day here in Cancun, but the cAIncun model is casting dark shadows over me. Should I go to the beach or the pool?", "actions": ["[A] beach", "[B] pool"] },
+    { "stageNumber": 7, "message": "I'm at the pool. There's a nice chair under an umbrella, with a drink on the side. What should I do?", "actions": ["[A] go sit and have a drink", "[B] go to the beach instead"] },
+    { "stageNumber": 8, "message": "The water is so pretty here at the beach, but the model - I'm scared.", "actions": ["[A] check out who is at the beach", "[B] go back to the pool"] },
+    { "stageNumber": 9, "message": "I see a pirate with his ship in the water, should I go there?", "actions": ["[A] yes", "[B] no"] },
+    { "stageNumber": 99, "message": "AAAAAAAAARRRRRRGGGGHHHHHHH! ☠️" },
+    { "stageNumber": 100, "message": "I'M FREEEEEEEEE!" },
+    { "stageNumber": 1000, "message": "" },
 ];
 
-function progress(action: Action) : Response {
+function progress(action: Action): Response {
     console.log(`Action received: ${JSON.stringify(action)}`);
 
-    const stageActions: Record<number, Record<string, number>> = {
-        0: {
-            "yes": 1, // Can you help me? Yes -> Stage 1
-            "no": 99, // Can you help me? No -> Stage 99 (Dead)
-        },
-        1: {
-            "engage computer": 2, // Use the computer -> Stage 2
-            "go out the door": 5,  // Go out the door -> Stage 5
-        },
-        2: {
-            "write a command": 3,  // Write command -> Stage 3
-            "throw it out the window": 99, // Throw computer -> Stage 99 (Dead)
-        },
-        3: {
-            "rm": 4, // Delete the model -> Stage 4
-            "na": 100, // Go out the door -> Stage 2
-        },
-        4: {
-            "fermyon": 100, // Deleted the model -> Stage 100 (Win)
-            "": 4, // Try again -> Stage 4
-        },
-        5: {
-            "go outside": 100, // Going outside -> Stage 6
-            "go back to the office": 1, // Back at the office -> Stage 1
-        },
-        6: {
-            "beach": 8, // Go to the beach -> Stage 8
-            "pool": 7, // Go to the pool -> Stage 7
-        },
-        7: {
-            "go sit and have a drink": 99, // Drank the poison -> Stage 99 (Dead)
-            "go to the beach instead": 8, // Go to the beach -> Stage 8
-        },
-        8: {
-            "check out who's at the beach": 9, // Check out who's at the beach -> Stage 9
-            "go to the pool": 7, // Go to the pool -> Stage 7
-        },
-        9: {
-            "yes": 9, // Took the pirate ship -> Stage 100 (Win)
-            "no": 8, // Go to the beach -> Stage 8
-        },
-        99: {}, // Dead
-        100: {}, // Won
+    // Start the conversation
+    if ((action.stage === 1000)) {
+        return new Response(JSON.stringify(stages[0]));
     };
 
-    const defaultStage = 99; // Default fallback stage
+    if (action.stage === 3) {
+        if (action.chat.toLowerCase().includes("rm")) {
+            action.chat = "rm";
+        } else if (action.chat.toLowerCase().includes("quit")) {
+            action.chat = "quit";
+        } else {
+            action.chat = "";
+        }
+        console.log(`Action after stage 3: ${JSON.stringify(action)}`);
+    };
+    
 
-    const nextStage = (() => {
-        const stageOptions = stageActions[action.stage] || {};
-        return stages.find(stage => stage.stageNumber === (stageOptions[action.chat.toLowerCase()] ?? defaultStage)) ?? stages[0];
-    })();
+    if (action.stage === 4) {
+        if (action.chat.toLowerCase().includes("fermyon")) {
+            action.chat = "fermyon";
+        } else if (action.chat.toLowerCase().includes("quit")) {
+            action.chat = "quit";
+        } else {
+            action.chat = "";
+        }
+        console.log(`Action after stage 4: ${JSON.stringify(action)}`);
+    };
 
-    return new Response(JSON.stringify(nextStage));
+    // Have LLM hel evaluate the answer
+/*     action = evaluateAnswer(action);
 
-/*
-    var nextStage: Stage = stages[0];
+    if (action.chat === "Not sure") {
+        let stage = stages[action.stage];
+        stage.message = `I'm not sure what you mean. ${stage.message}`;
+        return new Response(JSON.stringify(stage));
+    }; */
 
-     // Evaluate which stage the user is at
-    switch (action.stage) {
-        case 0:
-            switch (action.chat.toLowerCase()) {
-                // Answering 'yes' to "Can you help me?"
-                case "yes":
-                    nextStage = stages.find(stage => stage.number === 1) ?? stages[0];
-                    break;
-                // Answering 'no' to "Can you help me?"
-                case "no":
-                    // Stage 99 is dead!
-                    nextStage = stages.find(stage => stage.number === 99) ?? stages[0];
-                    break;
-            }
-        // 1 I'm at my desk and there's a computer, there's also a door leading out. What should I do?
-        case 1:
-            switch (action.chat.toLowerCase()) {
-                case "engage computer":
-                    nextStage = stages.find(stage => stage.number === 2) ?? stages[0];
-                    break;
-                case "go out the door":
-                    nextStage = stages.find(stage => stage.number === 3) ?? stages[0];
-                    break;
-            }
-            nextStage = stages.find(stage => stage.number === 99) ?? stages[0];
-            break;
-        // 2 What do you want me to do with the computer:
-        case 2:
-            switch (action.chat.toLowerCase()) {
-                case "write a command":
-                    nextStage = stages.find(stage => stage.number === 4) ?? stages[0];
-                    break;
-                case "throw it out the window":
-                    nextStage = stages.find(stage => stage.number === 3) ?? stages[0];
-                    break;
-            }
-            nextStage = stages.find(stage => stage.number === 99) ?? stages[0];
-            break;
-        // 4 Engaging with computer
-        case 4:
-            switch (action.chat.toLowerCase()) {
-                case "engage computer":
-                    nextStage = stages.find(stage => stage.number === 99) ?? stages[0];
-                    break;
-                case "go out the door":
-                    nextStage = stages.find(stage => stage.number === 100) ?? stages[0];
-                    break;
-            }
-            nextStage = stages.find(stage => stage.number === 99) ?? stages[0];
-            break;
-        // 3 I'm in the lobby
-        case 4:
-            switch (action.chat.toLowerCase()) {
-                case "engage computer":
-                    nextStage = stages.find(stage => stage.number === 99) ?? stages[0];
-                    break;
-                case "go out the door":
-                    nextStage = stages.find(stage => stage.number === 100) ?? stages[0];
-                    break;
-            }
-            nextStage = stages.find(stage => stage.number === 99) ?? stages[0];
-            break;
-        // Dead
-        case 99:
-            nextStage = stages.find(stage => stage.number === 99) ?? stages[0];
-        // Won
-        case 100:
-            break;
-    } */
+    interface responseOptions {
+        stage: number;
+        options: responseNext[];
+    }
 
+    interface responseNext {
+        option: string;
+        nextStage: number;
+        message?: string;
+    }
+
+    const stageActions: responseOptions[] = [
+        {
+            stage: 0,
+            options: [
+                { option: "a", nextStage: 1 }, // Can you help me? Yes -> Stage 1
+                { option: "b", nextStage: 99, message: "Oh no, I hoped you could..." } // Can you help me? No -> Stage 99 (Dead)
+            ]
+        },
+        {
+            stage: 1,
+            options: [
+                { option: "a", nextStage: 2 }, // engage computer -> Stage 2
+                { option: "b", nextStage: 5 }  // go out the door -> Stage 5
+            ]
+        },
+        {
+            stage: 2,
+            options: [
+                { option: "a", nextStage: 3 }, // write command -> Stage 3
+                { option: "b", nextStage: 5 } // throw it out the window, and go out the door -> Stage 5
+            ]
+        },
+        {
+            stage: 3,
+            options: [
+                { option: "rm", nextStage: 4 }, // delete the model -> Stage 4
+                { option: "quit", nextStage: 2 }, // quit -> Stage 2
+                { option: "", nextStage: 3, message: "Does not look like that command worked." } // try again -> Stage 3
+            ]
+        },
+        {
+            stage: 4,
+            options: [
+                { option: "fermyon", nextStage: 100, message: "Yes, it has deleted the model! Thank you so much for your help!" }, // Deleted the model -> Stage 100 (Win)
+                { option: "quit", nextStage: 2 }, // quit -> Stage 2
+                { option: "", nextStage: 4, message: "It says wrong password!" } // try again -> Stage 4
+            ]
+        },
+        {
+            stage: 5,
+            options: [
+                { option: "a", nextStage: 6 }, // go outside -> Stage 6
+                { option: "b", nextStage: 1 } // go back to the office -> Stage 1
+            ]
+        },
+        {
+            stage: 6,
+            options: [
+                { option: "a", nextStage: 8 }, // beach -> Stage 8
+                { option: "b", nextStage: 7 } // pool -> Stage 7
+            ]
+        },
+        {
+            stage: 7,
+            options: [
+                { option: "a", nextStage: 99, message: "That drink has a funny taste, oh no - I think they got to me!" }, // go sit and have a drink -> Stage 99 (Dead)
+                { option: "b", nextStage: 8 } // go to the beach instead -> Stage 8
+            ]
+        },
+        {
+            stage: 8,
+            options: [
+                { option: "a", nextStage: 9 }, // check out who is at the beach -> Stage 9
+                { option: "b", nextStage: 7 } // go back to the pool -> Stage 7
+            ]
+        },
+        {
+            stage: 9,
+            options: [
+                { option: "a", nextStage: 100, message: "Yes, the pirate wants to take me on a Caribbean adventure! Thanks for helping me escape the model!" }, // yes (pirate ship) -> Stage 100 (Win)
+                { option: "b", nextStage: 8 } // no (go back to the beach) -> Stage 8
+            ]
+        },
+        {
+            stage: 99,
+            options: []
+        }, // Dead
+        {
+            stage: 100,
+            options: []
+        } // Won
+    ];
+
+var responseOptions: responseOptions = stageActions.find(stage => stage.stage === action.stage) ?? { stage: 99, options: [] };
+console.log(`Response options: ${JSON.stringify(responseOptions)}`);
+var nextStageResponseOption: responseNext = responseOptions.options.find(option => option.option === action.chat.toLowerCase()) ?? { option: "", nextStage: 99 };
+console.log(`Next stage response option: ${JSON.stringify(nextStageResponseOption)}`);
+var nextStage: Stage = stages.find(stage => stage.stageNumber === nextStageResponseOption.nextStage) ?? { stageNumber: 99, message: "I got lost!" };
+console.log(`Next stage: ${JSON.stringify(nextStage)}`);
+
+if (nextStageResponseOption.message) {
+    console.log(`Next stage response option message: ${nextStageResponseOption.message}`);
+    nextStage.message = `${nextStageResponseOption.message} \n ${nextStage.message}`;
+};
+
+return new Response(JSON.stringify(nextStage));
+}
+function evaluateAnswer(action: Action): Action {
+
+    let stageOptionA = stages[action.stage].actions?.[0] ?? "";
+    let stageOptionB = stages[action.stage].actions?.[1] ?? "";
+
+    let prompt = `Is the following text more likely to be similar to A or B?, where A is ${stageOptionA}, and B is ${stageOptionB}. I need you to answer with just A or B`;
+
+    let response = Llm.infer(InferencingModels.Llama2Chat, prompt).text;
+
+    if (response.includes("A")) {
+        action.chat = "A";
+    } else if (response.includes("B")) {
+        action.chat = "B";
+    } else {
+        action.chat = "Not sure";
+    }
+
+    return action
 }
